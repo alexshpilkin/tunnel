114052777f738d75e8e5375a442592b829e3e1e5
Alexander Shpilkin <ashpilkin@gmail.com>
2020-02-09 17:10:02 +0300

    Determine upstream by Host header

--- tunnel.go
+++ tunnel.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
-func director(req *http.Request) {
+type handler struct {
+	Hosts map[string]string
+	proxy *httputil.ReverseProxy
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if _, ok := h.Hosts[req.Host]; !ok {
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusNotFound)
+		io.WriteString(w, `<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>tunnel</center>
+</body>
+</html>`)
+		return
+	}
+
+	if h.proxy == nil {
+		h.proxy = &httputil.ReverseProxy{Director: h.director}
+	}
+	h.proxy.ServeHTTP(w, req)
+}
+
+func (h *handler) director(req *http.Request) {
+	host := h.Hosts[req.Host]
 	req.URL.Scheme = "https"
-	req.URL.Host = "example.com"
-	req.Host = "example.com"
+	req.URL.Host = host
+	req.Host = host
 	if _, ok := req.Header["User-Agent"]; !ok {
 		req.Header.Set("User-Agent", "")
 	}
 }
 
 func main() {
-	proxy := &httputil.ReverseProxy{Director: director}
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+	handler := &handler{Hosts: map[string]string{
+		"localhost:8080": "example.com",
+	}}
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
